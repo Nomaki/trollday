cf359047d3456a633bb830777fa0f33c492c8a45
Romain <68727@supinfo.com>
2010-12-01 19:16:10 +0100

    Premier Commit
    Signed-off-by: Romain <68727@supinfo.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trollday::Application.config.secret_token = '2f306e4bdc93e7cc05e31ed19c067c265f02212eb63c3a398dda404f16a7d5dc60bdd5947111a6f707d8276c97bfe5e6c071612a6397852189366c15e57f4aa1'
