0ff8cf88195cc1e3e971db9cd1a3c5d7f63b718f
Romain <68727@supinfo.com>
2010-12-02 16:35:26 +0100

    PagesHelper
    Signed-off-by: Romain <68727@supinfo.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Trollday : One Day, One Troll"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
